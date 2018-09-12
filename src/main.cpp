411bb935c7a504c58f7236fd74350f8f37dda3ad
Jordan Poles <jpoles1@gmail.com>
2018-09-11 21:20:10 -0400

    Got the device working with wifi, but delayed before activation...

--- src/main.cpp
+++ src/main.cpp
@@ -1,121 +1,139 @@
+#include <string>
+using namespace std;
+
 #include <Arduino.h>
-
-/****************************************************************
-GestureTest.ino
-APDS-9960 RGB and Gesture Sensor
-Shawn Hymel @ SparkFun Electronics
-May 30, 2014
-https://github.com/sparkfun/APDS-9960_RGB_and_Gesture_Sensor
-
-Tests the gesture sensing abilities of the APDS-9960. Configures
-APDS-9960 over I2C and waits for gesture events. Calculates the
-direction of the swipe (up, down, left, right) and displays it
-on a serial console. 
-
-To perform a NEAR gesture, hold your hand
-far above the sensor and move it close to the sensor (within 2
-inches). Hold your hand there for at least 1 second and move it
-away.
-
-To perform a FAR gesture, hold your hand within 2 inches of the
-sensor for at least 1 second and then move it above (out of
-range) of the sensor.
-
-Hardware Connections:
-
-IMPORTANT: The APDS-9960 can only accept 3.3V!
- 
- Arduino Pin  APDS-9960 Board  Function
- 
- 3.3V         VCC              Power
- GND          GND              Ground
- A4           SDA              I2C Data
- A5           SCL              I2C Clock
- 2            INT              Interrupt
-
-****************************************************************/
-
 #include <Wire.h>
 #include <SparkFun_APDS9960.h>
+#include "ESP8266WiFi.h"
+#include <esp8266httpclient.h>
+
+//Private Config
+#include "config.h"
 
 // Pins
 #define APDS9960_SDA D1
 #define APDS9960_SCL D2
 #define APDS9960_INT D3
-
-// Constants
+#define ERROR_LED_PIN D4
 
 // Global Variables
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
 int isr_flag = 0;
 
 void interruptRoutine() {
-  isr_flag = 1;
+    isr_flag = 1;
+}
+
+void httpRequest(String apiURL){
+    if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+        http.begin(apiURL);
+        Serial.println("GET - " + apiURL);
+        int httpCode = http.GET();
+        Serial.println("Response Code - " + httpCode);
+        if(httpCode != 200){
+            digitalWrite(ERROR_LED_PIN, HIGH);
+            String payload = http.getString();
+            Serial.println("GET Error: " + payload);
+        }
+        else {
+            digitalWrite(ERROR_LED_PIN, LOW);
+        }
+    }
+    else {
+        Serial.println("Disconnected, cannot send request!");
+    }
+}
+
+void deviceCommandRequest(String device, String fname, String val){
+    String reqURL = String(SERVER_URL) + "?key=" + SERVER_KEY + "&device=" + device + "&fname=" + fname + "&val=" + val;
+    httpRequest(reqURL);
+}
+
+void wifiCheck(){
+    Serial.print("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+}
+
+void sendPowerCommands(String powerSetting) {
+    WiFi.begin(AP_SSID, AP_PASSWORD);
+    wifiCheck();
+    deviceCommandRequest("1", "power", powerSetting);
+    deviceCommandRequest("2", "power", powerSetting);
+    deviceCommandRequest("3", "power", powerSetting);
+    deviceCommandRequest("4", "power", powerSetting);
+    deviceCommandRequest("lightswitch", "power", powerSetting);
+    WiFi.disconnect();
 }
 
 void handleGesture() {
     if ( apds.isGestureAvailable() ) {
-    switch ( apds.readGesture() ) {
-      case DIR_UP:
-        Serial.println("UP");
-        break;
-      case DIR_DOWN:
-        Serial.println("DOWN");
-        break;
-      case DIR_LEFT:
-        Serial.println("LEFT");
-        break;
-      case DIR_RIGHT:
-        Serial.println("RIGHT");
-        break;
-      case DIR_NEAR:
-        Serial.println("NEAR");
-        break;
-      case DIR_FAR:
-        Serial.println("FAR");
-        break;
-      default:
-        Serial.println("NONE");
+        switch ( apds.readGesture() ) {
+        case DIR_UP:
+            Serial.println("UP");
+            break;
+        case DIR_DOWN:
+            Serial.println("DOWN");
+            break;
+        case DIR_LEFT:
+            Serial.println("LEFT");
+            sendPowerCommands("on");
+            break;
+        case DIR_RIGHT:
+            Serial.println("RIGHT");
+            sendPowerCommands("off");
+            break;
+        case DIR_NEAR:
+            Serial.println("NEAR");
+            break;
+        case DIR_FAR:
+            Serial.println("FAR");
+            break;
+        default:
+            Serial.println("NONE");
+        }
     }
-  }
 }
 
 void setup() {
-  Wire.begin(APDS9960_SDA, APDS9960_SCL);
+    Serial.begin(115200);
 
-  // Set interrupt pin as input
-  pinMode(APDS9960_INT, INPUT);
+    pinMode(ERROR_LED_PIN, OUTPUT);
+    digitalWrite(ERROR_LED_PIN, HIGH);
 
-  // Initialize Serial port
-  Serial.begin(9600);
-  Serial.println();
-  Serial.println(F("--------------------------------"));
-  Serial.println(F("SparkFun APDS-9960 - GestureTest"));
-  Serial.println(F("--------------------------------"));
-  
-  // Initialize interrupt service routine
-  attachInterrupt(APDS9960_INT, interruptRoutine, FALLING);
+    //Setup I2C
+    Wire.begin(APDS9960_SDA, APDS9960_SCL);
 
-  // Initialize APDS-9960 (configure I2C and initial values)
-  if ( apds.init() ) {
-    Serial.println(F("APDS-9960 initialization complete"));
-  } else {
-    Serial.println(F("Something went wrong during APDS-9960 init!"));
-  }
-  
-  // Start running the APDS-9960 gesture sensor engine
-  if ( apds.enableGestureSensor(true) ) {
-    Serial.println(F("Gesture sensor is now running"));
-  } else {
-    Serial.println(F("Something went wrong during gesture sensor init!"));
-  }
+    // Set interrupt pin as input
+    pinMode(APDS9960_INT, INPUT);
+
+    // Initialize interrupt service routine
+    attachInterrupt(APDS9960_INT, interruptRoutine, FALLING);
+
+    // Initialize APDS-9960 (configure I2C and initial values)
+    if ( apds.init() ) {
+        Serial.println(F("APDS-9960 initialization complete"));
+    } else {
+        Serial.println(F("Something went wrong during APDS-9960 init!"));
+    }
+
+    // Start running the APDS-9960 gesture sensor engine
+    if ( apds.enableGestureSensor(true) ) {
+        Serial.println(F("Gesture sensor is now running"));
+    } else {
+        Serial.println(F("Something went wrong during gesture sensor init!"));
+    }
 }
 
 void loop() {
-  if( isr_flag == 1 ) {
-    detachInterrupt(APDS9960_INT);
-    handleGesture();
-    isr_flag = 0;
-    attachInterrupt(APDS9960_INT, interruptRoutine, FALLING);
-  }
+    if( isr_flag == 1 ) {
+        detachInterrupt(APDS9960_INT);
+        handleGesture();
+        isr_flag = 0;
+        attachInterrupt(APDS9960_INT, interruptRoutine, FALLING);
+    }
 }
